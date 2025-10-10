5dc280d7145e9b786c69bd85757d8f9e87b48063
6scars <zasalatol@o2.pl>
2025-10-10 21:53:53 +0200

    setting jwt in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import {useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useState, useEffect } from "react";
+import {jwtDecode} from "jwt-decode"
 
 import "./App.css";
 import Login from "./Login";
@@ -7,7 +13,23 @@ import Register from "./Register";
 import WelcomePage from "./WelcomePage";
 
 function App() {
-  const [user, setUser] = useState({email:''});
+  const getUserInfo = () => {
+    const token = localStorage.getItem("jwt");
+    if(!token) return null;
+    try{
+      const decoded = jwtDecode(token);
+      if(!decoded || !decoded.exp) return false;
+      return decoded
+    }catch(err){
+      console.error("invalid token", err)
+      return false;
+    }
+  };
+
+
+
+  const [user, setUser] = useState(getUserInfo());
+
 
   return (
     <>
@@ -15,8 +37,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/register" />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login setUser={setUser}/>} />
-          <Route path="/welcomePage" element={<WelcomePage user={user} />}/>
+          <Route path="/login" element={<Login setUser={setUser} />} />
+          <Route path="/welcomePage" element={<WelcomePage user={user} />} />
         </Routes>
       </Router>
     </>
